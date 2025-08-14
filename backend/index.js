24ed22b6e84f1e7dd19730ba4de546bed0e73c23
Ömer <omer96@live.dk>
2025-08-14 21:18:31 +0200

    Fix URL

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const chatRoutes = require('./src/routes/chat');
 const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
-  'http://omer-ai-portfolio.netlify.app'
+  'https://omer-ai-portfolio.netlify.app'
 ];
 
 app.use(cors({
